af87e6f9fc3cfa04279db06ecfc92ebaadae8521
John Kamau <johnwaweru092@gmail.com>
2025-01-17 13:02:58 +0000

    casting of variables

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,18 @@
+#This is a comment
 print("Hello World")
+
+# VARIABLES
+# Variables are containers for storing data values
+x = 5
+y = 10
+
+# Variable Casting
+z = str('Hello')
+i = int(10)
+j = float(3)
+print(x)
+print(y)
+
+print(type(z))
+print(type(i))
+print(type(j))
