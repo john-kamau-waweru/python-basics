2bc2073cf93a6e6d72601a355417c3fa208b9b83
John Kamau <johnwaweru092@gmail.com>
2025-01-17 14:45:16 +0000

    looping in lists

--- lists.py
+++ lists.py
@@ -2,8 +2,56 @@
 # Lists are used to stroe multiple items in a single variable
 # Created using square brackets []
 
-basicList = ["apple", "banana", "banana", "orange", "apple"]
+basicList = ["apple", "banana", "banana", "orange", "apple", "pineapple", "avocado"]
 
 print(basicList)
 print(len(basicList))
 print(type(basicList))
+print(basicList[2:5])
+
+if "apple" in basicList:
+    print("Yes, apple is in the fruits list")
+
+# Lists are ordered(have indexes), changable and can have duplicates
+basicList[1] = "kiwi"
+print(basicList)
+
+# Add list items
+basicList.append("Watermelon") # add item at the end of a list
+print(basicList)
+
+basicList.insert(1, "berry")
+print(basicList)
+
+tropical = ["mango", "papaya"]
+basicList.extend(tropical)
+print(basicList)
+
+
+# Remove list items
+basicList.remove("papaya")
+print(basicList)
+
+basicList.pop(0)
+print(basicList)
+
+# Loop through a list
+
+for x in basicList:
+    print(x)
+
+print("******************************************************")
+
+print(range(len(basicList)))
+
+print("******************************************************")
+
+for i in range(len(basicList)):
+    print(basicList[i])
+
+print("******************************************************")
+
+m = 0
+while m < len(basicList):
+    print(basicList[m])
+    m = m + 1
