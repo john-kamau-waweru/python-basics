d3c0413043cac3b8033de2c64bbc0f3d3c36f5a4
John Kamau <johnwaweru092@gmail.com>
2025-01-17 14:12:56 +0000

    intro to lists

---
+++ lists.py
@@ -0,0 +1,9 @@
+# Python Lists
+# Lists are used to stroe multiple items in a single variable
+# Created using square brackets []
+
+basicList = ["apple", "banana", "banana", "orange", "apple"]
+
+print(basicList)
+print(len(basicList))
+print(type(basicList))
---
+++ operators.py
@@ -0,0 +1,9 @@
+# Python Operators
+# 1. Arithmentic
+# 2. Assignment
+# 3. Comparison
+# 4. Logical
+# 5. Identity
+# 6. Membership
+# 7. Bitwise
+
