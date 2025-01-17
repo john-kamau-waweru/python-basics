b4956e1a55bb1e5919e6fc785ebcd3db297fffb5
John Kamau <johnwaweru092@gmail.com>
2025-01-17 13:46:23 +0000

    strings modifications

---
+++ strings-data-type.py
@@ -0,0 +1,19 @@
+# Python Strings
+x = "Hello World"
+
+print(len(x))
+
+txt = "Python is awesome and easy to learn"
+print("awesome" in txt)
+
+# Slicing Strings
+a = "Hello World"
+print(a[2:5])
+
+# Modify strings
+simpleWord = " Simple Word "
+print(simpleWord.upper())
+print(simpleWord.lower())
+print(simpleWord.strip())
+print(simpleWord.replace("Word", "World"))
+print(simpleWord.strip().split(" "))
