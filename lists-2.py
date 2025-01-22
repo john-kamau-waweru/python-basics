aab869c00e9fa0f3a056d9aa01abb78ffff04bb6
John Kamau <johnwaweru092@gmail.com>
2025-01-22 11:34:54 +0000

    lists recap

--- lists-2.py
+++ lists-2.py
@@ -1,7 +1,4 @@
 fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+newList = [fruit for fruit in fruits if 'a' in fruit]
 
-newList = ['Fruit 1', 'Fruit 2']
-
-fruits[1:3] = newList
-
-print(fruits)
+freshList = fruits.copy()
