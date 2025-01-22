3b40a4c1adcb2442f60bb9630626268d25060997
John Kamau <johnwaweru092@gmail.com>
2025-01-22 11:49:50 +0000

    intro to tuples

---
+++ tuples.py
@@ -0,0 +1,7 @@
+# Tuples store a collection of data
+# Tuples are ordered and unchangeable and allow duplicate values
+# Tuples are written with round brackets
+
+fruits = ('apple', 'mango', 'banana')
+
+print(fruits[1])
