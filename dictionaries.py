3c237769450e9968659764218e3f67d1723fdc42
John Kamau <johnwaweru092@gmail.com>
2025-01-23 22:15:24 +0000

    intro to dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+# Python Dictionaties
+# Ordered, changable, do not allow duplicates
+# key:value pairs
+
+car = {
+    "color": "red",
+    "brand": "toyota",
+    "year": 2004,
+    "model": "premio"
+}
+
+for x, y in car.items():
+    print(x, y)
