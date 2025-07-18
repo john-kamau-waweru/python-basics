6766e25922078d3c0dbc6a7589129048ca3347fa
john-kamau01 <johnwaweru092@gmail.com>
2025-07-18 18:35:15 +0300

    simple operators

---
+++ lesson-1/operators.py
@@ -0,0 +1,10 @@
+meaning = 8
+print('')
+
+# if meaning > 10:
+#     print("Right ON!")
+# else:
+#     print("NOT TODAY")
+
+# Ternary Operator
+print("Right ON!") if meaning > 10 else print("NOT TODAY!")
